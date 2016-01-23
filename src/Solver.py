bfad1ff2fced7b851ba46bbab4dc5c2274306e1a
Punit Shetty <punitshetty30@gmail.com>
2016-01-22 23:18:07 -0800

    no change

--- src/Solver.py
+++ src/Solver.py
@@ -1,14 +1,2 @@
-import math
-import urllib
-
-
 class Solver:
-    def demo(self):
-        a = int(input("a "))
-        b = int(input("b "))
-        c = int(input("c "))
-        d = a + b + c
-        disc = math.sqrt(d)
-        print(disc)
-
-Solver().demo()
+    a = 5
