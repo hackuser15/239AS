8ed243843a02b9a90609b94a4b40eb77facac9fb
Punit Shetty <punitshetty30@gmail.com>
2016-01-30 18:43:27 -0800

    Added/Modified Functions and Plots

--- src/Functions.py
+++ src/Functions.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from matplotlib import pyplot
 from sklearn.cross_validation import cross_val_predict
 from sklearn.cross_validation import cross_val_score
 import matplotlib.pyplot as plt
@@ -7,35 +8,13 @@ from sklearn import cross_validation
 from sklearn.linear_model import LinearRegression
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import PolynomialFeatures
-
+import Plots
 
 def rootMeanSquareError(x, y):
     return np.sqrt(np.mean((x - y) ** 2))
 
-def printFeatureCoefficients(x, y):
-    zipped=list(zip(x, y))
-    print(pd.DataFrame(data=zipped,columns=['Features','Coefficents']))
-
-def crossValScoreRMSE(scores):
-    MSE = -scores
-    return np.sqrt(np.mean(MSE))
-
-def callClassifier(obj,X_train,y_train,X_test,y_test,feature_cols, label):
-    obj.fit(X_train, y_train)
-    printFeatureCoefficients(feature_cols, obj.coef_)
-    pred=obj.predict(X_test)
-    print('%s - Root Mean Squared Error: %.2f' % (label, rootMeanSquareError(pred, y_test)))
-    print('%s - Variance score: %.2f' % (label, obj.score(X_test, y_test)))
-    plt.scatter(obj.predict(X_test), y_test,  color='green')
-
-def callCrossVal(obj,X,y,numberoffolds,label):
-    predicted = cross_val_predict(obj, X, y, cv=numberoffolds)
-    scores = cross_val_score(obj, X, y, cv=numberoffolds, scoring='mean_squared_error')
-    #print(scores)
-    print('%s - The best RMSE obtained with CV is: %.2f' % bestRMSE(scores))
-    #print('%s - Mean Squared Error with CV %.2f ' % (label, np.mean(-scores)))
-    #print('%s - Root Mean Squared Error with CV %.2f ' % (label, np.sqrt(np.mean(-scores))))
-    return bestRMSE(scores)
+def avgRMSE(scores):
+    return np.mean(np.sqrt(-scores))
 
 def bestRMSE(scores):
     MSE = np.sqrt(-scores)
@@ -44,25 +23,65 @@ def bestRMSE(scores):
 def minRMSE(rmseList):
     return np.amin(rmseList)
 
-def polynomialRegression(obj,X_train,y_train,X_test,y_test, label):
+def crossValScoreRMSE(scores):
+    MSE = -scores
+    return np.sqrt(np.mean(MSE))
+
+def printFeatureCoefficients(x, y):
+    zipped=list(zip(x, y))
+    print(pd.DataFrame(data=zipped,columns=['Features','Coefficents']))
+
+def callClassifier(obj,X_train,y_train,X_test,y_test,feature_cols, label):
+    obj.fit(X_train, y_train)
+    pred=obj.predict(X_test)
+    print('%s - Root Mean Squared Error: %.4f' % (label, rootMeanSquareError(pred, y_test)))
+    print('%s - Variance score: %.4f' % (label, obj.score(X_test, y_test)))
+
+def callClassifierFeatures(obj,X_train,y_train,X_test,y_test,feature_cols, label):
+    obj.fit(X_train, y_train)
+    printFeatureCoefficients(feature_cols, obj.coef_)
+    pred=obj.predict(X_test)
+    print('%s - Root Mean Squared Error: %.4f' % (label, rootMeanSquareError(pred, y_test)))
+    #print('%s - Variance score: %.4f' % (label, obj.score(X_test, y_test)))
+
+def callCrossVal(obj,X,y,numberoffolds,label):
+    predicted = cross_val_predict(obj, X, y, cv=numberoffolds)
+    scores = cross_val_score(obj, X, y, cv=numberoffolds, scoring='mean_squared_error')
+    print('%s - Averaged RMSE with CV %.4f ' % (label, np.mean(np.sqrt(-scores))))
+    print('%s - Best RMSE with CV is: %.4f' % (label,bestRMSE(scores)))
+    return bestRMSE(scores)
+
+def callCrossValPoly(obj,X,y,numberoffolds,degree,label):
+    predicted = cross_val_predict(obj, X, y, cv=numberoffolds)
+    scores = cross_val_score(obj, X, y, cv=numberoffolds, scoring='mean_squared_error')
+    print('%s - The best RMSE obtained with CV for degree %d is: %.4f' % (label,degree,bestRMSE(scores)))
+    return bestRMSE(scores)
+
+def polynomialRegression(obj,X_train,y_train,X_test,y_test, degree, label):
     rmseList = []
-    for degree in [1, 2, 3, 4, 5, 6]:
+    degreeList = []
+    for degree in range(1,degree+1):
         model = make_pipeline(PolynomialFeatures(degree), obj)
         model.fit(X_train, y_train)
         pred=model.predict(X_test)
         rmse=rootMeanSquareError(pred, y_test)
+        degreeList.append(degree)
         rmseList.append(rmse)
-        print("Root Mean Squared Error: (Polynomial): %.2f" % rmse)
-        print('Variance score (Polynomial): %.2f' % model.score(X_test, y_test))
-    print('The best RMSE obtained is: %.2f for Degree: %d' % (minRMSE(rmseList),rmseList.index(minRMSE(rmseList))+1))
+        print("Root Mean Squared Error: (Polynomial): %.4f" % rmse)
+        print('Variance score (Polynomial): %.4f' % model.score(X_test, y_test))
+    print('The best RMSE obtained is: %.4f for Degree: %d' % (minRMSE(rmseList),rmseList.index(minRMSE(rmseList))+1))
+    Plots.scatterPlot(degreeList, rmseList, 'Degree_of_Polynomial', 'RMSE', 'Title', 'green', 'polyreg')
 
-def polynomialRegressionCV(obj,X,y,numberoffolds,label):
+def polynomialRegressionCV(obj,X,y,numberoffolds,degree,label):
     rmseList = []
-    for degree in [1, 2, 3, 4, 5, 6]:
+    degreeList = []
+    for degree in range(1,degree+1):
         model = make_pipeline(PolynomialFeatures(degree), obj)
-        rmse = callCrossVal(model, X, y, 10, 'Linear Regression Poly')
+        rmse = callCrossValPoly(model, X, y, numberoffolds, degree, 'Linear Regression Poly')
+        degreeList.append(degree)
         rmseList.append(rmse)
-    print('The best RMSE obtained is: %.2f for Degree: %d' % (minRMSE(rmseList),rmseList.index(minRMSE(rmseList))+1))
+    print('The best RMSE obtained is: %.4f for Degree: %d' % (minRMSE(rmseList),rmseList.index(minRMSE(rmseList))+1))
+    Plots.scatterPlot(degreeList, rmseList, 'Degree_of_Polynomial', 'RMSE', 'Title', 'red', 'polyregCV')
 
 def plotWorkFlow(data, num, label):
     workflow = data[data['Work-Flow-ID=work_flow_' + str(num)] == 1]
--- src/Plots.py
+++ src/Plots.py
@@ -2,9 +2,21 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def scatterPlot(x, y, xlabel, ylabel, title, color):
-    plt.scatter(x, y, color)
+def scatterPlot(x, y, xlabel, ylabel, title, c, name):
+    plt.scatter(x, y, color=c)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.title(title)
     plt.show()
+    plt.savefig(name+'.png')
+    plt.clf()
+
+def residualPlot(x, y, xlabel, ylabel, title, c, name):
+    plt.scatter(x, y, color=c)
+    plt.hlines(y=0,xmin=0,xmax=50)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(title)
+    plt.show()
+    plt.savefig(name+'.png')
+    plt.clf()
