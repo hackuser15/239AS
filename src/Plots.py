8650e8d4a9a5b53db27fb497b8ab2cfb23e8f386
Akshay Dattatray Shinde <Akshay Dattatray Shinde>
2016-01-31 15:51:41 -0800

    Changed Residual Plot Horizontal Line xmax

--- src/Plots.py
+++ src/Plots.py
@@ -13,7 +13,7 @@ def scatterPlot(x, y, xlabel, ylabel, title, c, name):
 
 def residualPlot(x, y, xlabel, ylabel, title, c, name):
     plt.scatter(x, y, color=c)
-    plt.hlines(y=0,xmin=0,xmax=50)
+    plt.hlines(y=0,xmin=0,xmax=max(x))
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.title(title)
@@ -37,5 +37,6 @@ def plotWorkFlow(data, num, label):
     plt.xlabel('Operations')
     plt.ylabel('Size of Backup (GB)')
     plt.title('Workflow '+ str(num)+ ' ' + label)
+    # plt.show()
     plt.savefig('Workflow_' + str(num) + '_' + label + '.png')
     plt.clf()
