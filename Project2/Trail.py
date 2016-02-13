a31e1cdd970a5588678cb65584764305b491031c
Avneet <avneetoberoi15@gmail.com>
2016-02-13 15:52:04 -0800

    Part 1b)

--- Project2/Trail.py
+++ Project2/Trail.py
@@ -1,5 +1,13 @@
 from sklearn.datasets import fetch_20newsgroups
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction import text
+import string
 import Plots
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem.wordnet import WordNetLemmatizer
+
 
 categories = ['comp.graphics','comp.os.ms-windows.misc','comp.sys.ibm.pc.hardware','comp.sys.mac.hardware','rec.autos','rec.motorcycles','rec.sport.baseball','rec.sport.hockey']
 
@@ -10,6 +18,7 @@ train_1 = fetch_20newsgroups(subset='train', categories = categories, shuffle = 
 length = len(train_1.data)
 #print("Total records = ",len(train_1.data))
 
+
 #print(len(train_1))
 #print(train_1.target_names)
 
@@ -19,6 +28,9 @@ category_count = {}
 
 computer_count = 0
 recreational_count = 0
+lmtzr = WordNetLemmatizer()
+
+#print(string.punctuation)
 
 for i in range(0,length):
     category = train_1.target_names[train_1.target[i]]
@@ -37,6 +49,43 @@ for i in range(0,length):
 print("Computer category count= "+str(computer_count))
 print("Recreational category count= "+str(recreational_count))
 Plots.barPlot(category_count)
+
+####################################################################################
+#Removing punctuations
+#Tokenize string
+stop = stopwords.words('english')
+terms = []
+
+def cleanDoc(doc):
+    #print("Original\n",doc)
+    cleaned =""
+    for i in word_tokenize(doc.lower()):
+        if i not in stop:
+            root = lmtzr.lemmatize(i)
+            if root not in string.punctuation:
+                cleaned = cleaned+ " "+root
+    return cleaned
+    #print("CLEANED\n", cleaned)
+
+
+for i in range(0, length):
+    cleaned_doc = cleanDoc(train_1.data[i])
+    train_1.data[i] = cleaned_doc
+
+
+
+vectorizer = TfidfVectorizer()
+vectors = vectorizer.fit_transform(train_1.data)
+print("Term Count = ",vectors.shape[1])
+
+
+
+
+
+
+#print("\n".join(train_1.data[2].split("\n")[:10]))
+
+#print(text.ENGLISH_STOP_WORDS)
 
 
 #print(list(category_count.keys()))
