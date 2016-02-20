ec22e2ff0d0bb28cb39cb12eff690ae9dc416124
Avneet <avneetoberoi15@gmail.com>
2016-02-20 15:59:48 -0800

    Changes

--- Project2/Main.py
+++ Project2/Main.py
@@ -21,14 +21,17 @@ computer_count = 0
 recreational_count = 0
 categories = ['comp.graphics','comp.os.ms-windows.misc','comp.sys.ibm.pc.hardware','comp.sys.mac.hardware','rec.autos','rec.motorcycles','rec.sport.baseball','rec.sport.hockey']
 
-train_1 = fetch_20newsgroups(subset='train', categories = categories, shuffle = True, random_state=42)
-length = len(train_1.data)
+train_data = fetch_20newsgroups(subset='train', categories = categories, shuffle = True, random_state=42)
+train_all  = fetch_20newsgroups(subset='train', shuffle = True, random_state=42)
+test_data = fetch_20newsgroups(subset='test',categories = categories, shuffle = True, random_state=42)
+
+length = len(train_data.data)
 category_count = {}
 #
 
 
 for i in range(0,length):
-    category = train_1.target_names[train_1.target[i]]
+    category = train_data.target_names[train_data.target[i]]
     if(category in category_count):
         category_count[category] = category_count[category] + 1
     else:
@@ -44,39 +47,38 @@ print("Recreational category count= "+str(recreational_count))
 Plots.barPlot(category_count)
 
 ########################## END OF A ####################################
-train_all = fetch_20newsgroups(subset='train', shuffle = True, random_state=42)
-no_of_docs = len(train_all.data)
+no_of_docs = len(train_data.data)
 
 #Clean all documents
 for i in range(0, no_of_docs):                   #Change this to no_of_docs
-    cleaned_doc = Functions.cleanDoc(train_all.data[i])
-    train_all.data[i] = cleaned_doc
+    cleaned_doc = Functions.cleanDoc(train_data.data[i])
+    train_data.data[i] = cleaned_doc
 
 
 #Convert to TfIdfVector
 vectorizer = TfidfVectorizer()
-vectors = vectorizer.fit_transform(train_all.data)
+vectors = vectorizer.fit_transform(train_data.data)
 
 print("Term Count = ",vectors.shape[1])
 
 #Cleaning the test data as well
-test_all = fetch_20newsgroups(subset='test', shuffle = True, random_state=42)
-no_of_test_docs = len(test_all.data)
+no_of_test_docs = len(test_data.data)
 
 #Clean all documents
 for i in range(0, no_of_test_docs):                   #Change this to no_of_docs
-    cleaned_doc = Functions.cleanDoc(test_all.data[i])
-    test_all.data[i] = cleaned_doc
+    cleaned_doc = Functions.cleanDoc(test_data.data[i])
+    test_data.data[i] = cleaned_doc
 
 
 ########################## END OF B ####################################
+
 categories = ['comp.sys.ibm.pc.hardware', 'comp.sys.mac.hardware', 'misc.forsale', 'soc.religion.christian']
 indices = []
 for i in categories:
     indices.append(train_all.target_names.index(i))
 
 #Generating term frequency vector for all documents
-count_vect = CountVectorizer()
+count_vect = CountVectorizer(stop_words="english")
 X_train_counts = count_vect.fit_transform(train_all.data)
 #print(count_vect.get_feature_names()) Will print names of all final terms
 
@@ -134,61 +136,61 @@ svd_transformer = Pipeline([('tfidf', vectorizer),
 svd_matrix = svd_transformer.fit_transform(train_data.data)
 
 ########################## END OF D ####################################
-#Support Vector Machine
-print("Support Vector Machine Analysis")
-svc = SVC(kernel='linear', probability=True, random_state=40) #decision_function_shape='ovo',max_iter=5
-Functions.newsGroupClassifier(svd_transformer,svc,train_data,test_data,'Support Vector Machine')
-
-########################## END OF E ####################################
-#Soft margin SVM
-
-# Grid Search
-print("Soft Margin SVM Analysis")
-pipeline = Pipeline([
-    ('vect', svd_transformer),
-    ('svmobj', svc),
-])
-parameters = {
-    'svmobj__gamma': [1e-3, 1e3]
-}
-if __name__ == "__main__":
-        grid_search = GridSearchCV(pipeline, parameters, cv=5) #n_jobs=1
-        grid_search.fit(train_data.data, train_data.target)
-        predicted = grid_search.predict(test_data.data)
-        calcPrintResults(test_data,predicted,'Soft Margin SVM')
-        print("Best parameters set:")
-        best_parameters = grid_search.best_estimator_.get_params()
-        for param_name in sorted(parameters.keys()):
-            print("\t%s: %r" % (param_name, best_parameters[param_name]))
-
-########################## END OF F ####################################
-#Naive Bayes
-print("Naive Bayes Analysis")
-#naivebayes = MultinomialNB()
-naivebayes = GaussianNB()
-Functions.newsGroupClassifier(svd_transformer,naivebayes,train_data,test_data,'Naive Bayes')
-
-########################## END OF G ####################################
-#Logistic Regression
-print("Logistic Regression Analysis")
-logistic = LogisticRegression(penalty='l2', max_iter=5, random_state=40)
-Functions.newsGroupClassifier(svd_transformer,logistic,train_data,test_data,'Logistic Regression')
-
-########################## END OF H ####################################
-#MultiClass Classification
-#Probably repeat the entire process from svd
-
-categories = ['comp.sys.ibm.pc.hardware' , 'comp.sys.mac.hardware', 'misc.forsale', 'soc.religion.christian']
-train_data = fetch_20newsgroups(subset='train',categories=categories, shuffle=True, random_state=40)
-test_data = fetch_20newsgroups(subset='test',categories=categories, shuffle=True, random_state=40)
-print("OneVsRestClassifier Analysis")
-obj = OneVsRestClassifier(svc)
-Functions.newsGroupMultiClassifier(svd_transformer, obj, train_data, test_data, 'OneVsRestClassifier')
-print("OneVsOneClassifier Analysis")
-svc = SVC(kernel='linear',class_weight='balanced',probability=True,random_state=40)
-obj = OneVsOneClassifier(svc)
-Functions.newsGroupMultiClassifier(svd_transformer, obj, train_data, test_data, 'OneVsOneClassifier')
-
-########################## END OF I ####################################
-
-
+# #Support Vector Machine
+# print("Support Vector Machine Analysis")
+# svc = SVC(kernel='linear', probability=True, random_state=40) #decision_function_shape='ovo',max_iter=5
+# Functions.newsGroupClassifier(svd_transformer,svc,train_data,test_data,'Support Vector Machine')
+#
+# ########################## END OF E ####################################
+# #Soft margin SVM
+#
+# # Grid Search
+# print("Soft Margin SVM Analysis")
+# pipeline = Pipeline([
+#     ('vect', svd_transformer),
+#     ('svmobj', svc),
+# ])
+# parameters = {
+#     'svmobj__gamma': [1e-3, 1e3]
+# }
+# if __name__ == "__main__":
+#         grid_search = GridSearchCV(pipeline, parameters, cv=5) #n_jobs=1
+#         grid_search.fit(train_data.data, train_data.target)
+#         predicted = grid_search.predict(test_data.data)
+#         calcPrintResults(test_data,predicted,'Soft Margin SVM')
+#         print("Best parameters set:")
+#         best_parameters = grid_search.best_estimator_.get_params()
+#         for param_name in sorted(parameters.keys()):
+#             print("\t%s: %r" % (param_name, best_parameters[param_name]))
+#
+# ########################## END OF F ####################################
+# #Naive Bayes
+# print("Naive Bayes Analysis")
+# #naivebayes = MultinomialNB()
+# naivebayes = GaussianNB()
+# Functions.newsGroupClassifier(svd_transformer,naivebayes,train_data,test_data,'Naive Bayes')
+#
+# ########################## END OF G ####################################
+# #Logistic Regression
+# print("Logistic Regression Analysis")
+# logistic = LogisticRegression(penalty='l2', max_iter=5, random_state=40)
+# Functions.newsGroupClassifier(svd_transformer,logistic,train_data,test_data,'Logistic Regression')
+#
+# ########################## END OF H ####################################
+# #MultiClass Classification
+# #Probably repeat the entire process from svd
+#
+# categories = ['comp.sys.ibm.pc.hardware' , 'comp.sys.mac.hardware', 'misc.forsale', 'soc.religion.christian']
+# train_data = fetch_20newsgroups(subset='train',categories=categories, shuffle=True, random_state=40)
+# test_data = fetch_20newsgroups(subset='test',categories=categories, shuffle=True, random_state=40)
+# print("OneVsRestClassifier Analysis")
+# obj = OneVsRestClassifier(svc)
+# Functions.newsGroupMultiClassifier(svd_transformer, obj, train_data, test_data, 'OneVsRestClassifier')
+# print("OneVsOneClassifier Analysis")
+# svc = SVC(kernel='linear',class_weight='balanced',probability=True,random_state=40)
+# obj = OneVsOneClassifier(svc)
+# Functions.newsGroupMultiClassifier(svd_transformer, obj, train_data, test_data, 'OneVsOneClassifier')
+#
+# ########################## END OF I ####################################
+#
+#
