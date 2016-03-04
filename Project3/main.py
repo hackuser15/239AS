dee115da752fac174501a72bb3e096b1d9d8070b
Akshay Dattatray Shinde <Akshay Dattatray Shinde>
2016-03-04 11:42:11 -0800

    Q4

--- Project3/main.py
+++ Project3/main.py
@@ -103,3 +103,82 @@ print(recall)
 plotROC(precision,recall,'ROC')
 # print(len(precision))
 # print(len(recall))
+
+#Q4
+R_new, W_new = weights, matrix
+
+for k in [10, 50, 100]:
+    print('k: {}'. format(k))
+    U, V = nmfw(R_new, W_new, k)
+    R_hat = np.dot(U, V)
+
+k = 100
+n_iterations = 20
+for lambda_ in [0.01, 0.1, 1]:
+    print('lambda: {}'. format(lambda_))
+    R_hat = weightedRegALS(R_new, lambda_, k, W_new, n_iterations)
+
+lambda_ = 0.1
+from sklearn.cross_validation import KFold
+kf = KFold(len(ratings), n_folds=10)
+# print(len(kf))
+scores = []
+precision = []
+recall = []
+loop_no = 1
+for train_index, test_index in kf:
+    print("CROSS VALIDATION : %s" %loop_no)
+    #print("TRAIN:", train_index, "TEST:", test_index)
+    weights = convertToMatrixKF(ratings,train_index)
+    prec_k = []
+    rec_k = []
+    #call func
+
+    U, V = weightedRegALS(weights, lambda_, k, matrix, n_iterations)
+    res_matrix = np.dot(U, V)
+    test_list = []
+    test_res_list = []
+    for index in test_index:
+        user = ratings.iloc[index]['user_id']-1
+        movie = ratings.iloc[index]['movie_id']-1
+        test_list.append(matrix[user, movie])
+        test_res_list.append(res_matrix[user, movie])
+    test_matrix = np.array(test_list)
+    test_res_matrix = np.array(test_res_list)
+    # print(test_matrix.shape)
+    # print(test_res_matrix.shape)
+    meanall = np.mean(np.absolute(np.subtract(res_matrix,matrix)))
+    mean = np.mean(np.absolute(np.subtract(test_res_matrix,test_matrix)))
+    print("AVG ERROR IN FULL MATRIX %s:" %meanall)
+    print("AVG ERROR FOR TEST DATA IN THIS FOLD %s:" %mean)
+    scores.append(mean)
+
+    for k in [3, 3.5, 4]:
+        predtest = np.where(test_matrix>k)
+        predtest = np.array(predtest)
+        predtestres = np.where(test_res_matrix>k)
+        predtestres = np.array(predtestres)
+        c = np.in1d(predtest,predtestres)
+        #print(c)
+        intersection = np.count_nonzero(c)
+        #print(predtest.size)
+        #print(predtestres.size)
+        if predtest.size == 0:
+            prec,rec = 0.0,0.0
+        else:
+            prec = intersection/predtestres.size
+            rec = intersection/predtest.size
+        print("Precision: %s" % prec)
+        print("Recall: %s" % rec)
+        prec_k.append(prec)
+        rec_k.append(rec)
+        precision.append(prec)
+        recall.append(rec)
+    plotROC(prec_k,rec_k,'ROC_Regularized_Fold_'+str(loop_no))
+    loop_no = loop_no + 1
+print("MIN ERROR IN 10 FOLD %s:" %np.amin(scores))
+print("MAX ERROR IN 10 FOLD %s:" %np.amax(scores))
+print("AVG ERROR IN 10 FOLD %s:" %np.mean(scores))
+print(precision)
+print(recall)
+plotROC(precision,recall,'ROC_Regularized_Final')
