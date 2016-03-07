5bd5f14e4730bd03b8fa1766f74e098e64596234
AkshayDattatray <shindeak@cs.ucla.edu>
2016-03-06 17:25:49 -0800

    Q1 statistics

--- Project4/Functions.py
+++ Project4/Functions.py
@@ -1,65 +1,74 @@
+import os
+import json
 import datetime
 import matplotlib.pyplot as plt
 import numpy as np
 from datetime import timedelta
 
-def getHashTagStats(hashtag_data, hashtag):
+script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
+
+def getHashTagStats(hashtag):
+    tweet_path = "tweet_data/tweets_#" + hashtag + ".txt"
+    abs_tweet_path = os.path.join(script_dir, tweet_path)
     n_followers = 0.0
     n_retweets = 0.0
     n_tweets = 0.0
+    d = {}
 
-    start_date = datetime.datetime.fromtimestamp(hashtag_data[0]['firstpost_date'])
-    end_date = datetime.datetime.fromtimestamp(datetime.MINYEAR)
-
+    start_time = datetime.datetime.now()
+    end_time = datetime.datetime.fromtimestamp(datetime.MINYEAR)
     user_ids = set([])
-    for tweet in hashtag_data:
+    for line in open(abs_tweet_path, encoding="utf8"):
+        tweet=json.loads(line)
         n_tweets += 1
         n_retweets += tweet['metrics']['citations']['data'][0]['citations']
         tweet_time = datetime.datetime.fromtimestamp(tweet['firstpost_date'])
-        if tweet_time < start_date:
-            start_date = tweet_time
-        if tweet_time > end_date:
-            end_date = tweet_time
+        if tweet_time < start_time:
+            start_time = tweet_time
+        if tweet_time > end_time:
+            end_time = tweet_time
         user_id = tweet['tweet']['user']['id']
         followers = tweet['author']['followers']
         if user_id not in user_ids:
             user_ids.add(user_id)
             n_followers += followers
+        if hashtag in ('nfl','superbowl'):
+            tweet_time = tweet['firstpost_date']
+            tweet_time = datetime.datetime.fromtimestamp(tweet_time)
+            time_key = datetime.datetime(tweet_time.year, tweet_time.month, tweet_time.day, tweet_time.hour, 0, 0)
+            if time_key not in d:
+                d[time_key] = 1
+            else:
+                d[time_key] += 1
+    print('-----------------------------------------')
     print('Statistics for %s:'%hashtag)
-    print('First tweet time: '+ start_date.strftime('%Y-%m-%d %H:%M:%S'))
-    print('Last tweet time: '+ end_date.strftime('%Y-%m-%d %H:%M:%S'))
-    n_hours = int((end_date - start_date).total_seconds()/3600 + 0.5)
+    print('-----------------------------------------')
+    print('First tweet time: '+ start_time.strftime('%Y-%m-%d %H:%M:%S'))
+    print('Last tweet time: '+ end_time.strftime('%Y-%m-%d %H:%M:%S'))
+    n_hours = int((end_time - start_time).total_seconds()/3600 + 0.5)
 
-    # print("total number of hour: ", n_hours)
+    # print("total number of hours: ", n_hours)
     # print("total number of users posting this hashtag: ", len(user_ids))
     # print("total number of tweets containing this hashtag: ", n_tweets)
     # print("total number of retweets: ", n_retweets)
-    print('-----------------------------------------')
+
     print("Average number of tweets per hour: ", n_tweets/n_hours)
     print("Average number of followers of users posting the tweets: ", n_followers/len(user_ids))
     print("Average number of retweets: ", n_retweets/n_tweets)
-    print('-----------------------------------------')
 
-def plotHistogram(hashtag_data, hashtag):
-    d = {}
-    for tweet in hashtag_data:
-        tweet_time = tweet["firstpost_date"]
-        tweet_time = datetime.datetime.fromtimestamp(tweet_time)
-        time_key = datetime.datetime(tweet_time.year, tweet_time.month, tweet_time.day, tweet_time.hour, 0, 0)
-        if time_key not in d:
-            d[time_key] = 1
-        else:
-            d[time_key] += 1
+    if hashtag in ('nfl','superbowl'):
+        plotHistogram(d, hashtag)
 
-    start_time = min(d.keys())
-    end_time = max(d.keys())
+def plotHistogram(tweetTimeDict, hashtag):
+    start_time = min(tweetTimeDict.keys())
+    end_time = max(tweetTimeDict.keys())
 
     tweets_per_hour = []
 
     cur = start_time
     while cur <= end_time:
-        if cur in d:
-            tweets_per_hour.append(d[cur])
+        if cur in tweetTimeDict:
+            tweets_per_hour.append(tweetTimeDict[cur])
         else:
             tweets_per_hour.append(0)
 
--- Project4/main.py
+++ Project4/main.py
@@ -11,34 +11,10 @@ start_time = time.time()
 script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
 
 hashtags = ["gohawks","gopatriots","nfl","patriots","sb49","superbowl"]
-hashtag_data = {}
-
-# for hashtag in hashtags:
-#     tweet_data = []
-#     tweet_path = "tweet_data/tweets_#" + hashtag + ".txt"
-#     abs_tweet_path = os.path.join(script_dir, tweet_path)
-#     for line in open(abs_tweet_path, encoding="utf8"):
-#         tweet_data.append(json.loads(line))
-#     hashtag_data[hashtag] = tweet_data
 
 #Q.1
 print("--------------Q1----------------")
 for hashtag in hashtags:
-    tweet_data = []
-    tweet_path = "tweet_data/tweets_#" + hashtag + ".txt"
-    abs_tweet_path = os.path.join(script_dir, tweet_path)
-    for line in open(abs_tweet_path, encoding="utf8"):
-        tweet_data.append(json.loads(line))
-    hashtag_data[hashtag] = tweet_data
-    getHashTagStats(hashtag_data[hashtag], hashtag)
-
-for hashtag in ["nfl","superbowl"]:
-    tweet_data = []
-    tweet_path = "tweet_data/tweets_#" + hashtag + ".txt"
-    abs_tweet_path = os.path.join(script_dir, tweet_path)
-    for line in open(abs_tweet_path, encoding="utf8"):
-        tweet_data.append(json.loads(line))
-    hashtag_data[hashtag] = tweet_data
-    plotHistogram(hashtag_data[hashtag], hashtag)
+    getHashTagStats(hashtag)
 
 print("--- %s seconds ---" % (time.time() - start_time))
