1690fe012592f8bfb706b2e8ab508bd36b8d4c73
Punit Shetty <punitshetty30@gmail.com>
2016-03-17 23:47:58 -0700

    Modified Functions.py and main.py

--- Project4/main.py
+++ Project4/main.py
@@ -1,6 +1,6 @@
 #https://ucla.box.com/s/nv9td9kvvfvg3tya0dlvbs1kn5o87gmv
 #Download tweet data from above mentioned url and folder in tweet_data in project directory
-
+#Download test data and put the sample files of test data into the folder test_data
 import time
 
 from sklearn.ensemble import RandomForestRegressor
